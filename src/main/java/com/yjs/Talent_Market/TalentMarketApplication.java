58040a4cee3bc7d1bafee35810bf4e382a5f0792
dbwnstjs4862 <dbwnstjs4862@gmail.com>
2024-06-04 16:47:11 +0900

    project 시작

---
+++ src/main/java/com/yjs/Talent_Market/TalentMarketApplication.java
@@ -0,0 +1,13 @@
+package com.yjs.Talent_Market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yjs/Talent_Market/TalentMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjs.Talent_Market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
